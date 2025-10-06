ea63bd00234da623cc8a3d82edb328f4762af345
Jocelyn Flament <jocelyn.flament@gmail.com>
2025-10-06 10:14:36 +0200

    ajout du code des classes Olympic et Participation

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -7,3 +7,22 @@ example of an olympic country:
     participations: []
 }
 */
+
+import { Participation } from './Participation';
+
+export class Olympic {
+    constructor(
+        public id: number,
+        public country: string,
+        public participations: Participation[]
+    ) {}
+
+    static from(obj: Partial<Olympic>): Olympic {
+        const parts = (obj.participations ?? []).map(p => Participation.from(p as Partial<Participation>));
+        return new Olympic(
+            obj.id ?? 0,
+            obj.country ?? '',
+            parts
+        );
+    }
+}
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -9,3 +9,23 @@ example of participation:
     athleteCount: 372
 }
 */
+
+export class Participation {
+    constructor(
+        public id: number,
+        public year: number,
+        public city: string,
+        public medalsCount: number,
+        public athleteCount: number
+    ) {}
+
+    static from(obj: Partial<Participation>): Participation {
+        return new Participation(
+            obj.id ?? 0,
+            obj.year ?? 0,
+            obj.city ?? '',
+            obj.medalsCount ?? 0,
+            obj.athleteCount ?? 0
+        );
+    }
+}
